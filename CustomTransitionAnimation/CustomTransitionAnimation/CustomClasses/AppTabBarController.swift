faeb3e9c4d14a99f17b52d321ccd4267659d1033
Ravi <ravi.jangid@anktech.co.in>
2020-08-02 16:25:38 +0530

    Added TabBar Shadow

--- CustomTransitionAnimation/CustomTransitionAnimation/CustomClasses/AppTabBarController.swift
+++ CustomTransitionAnimation/CustomTransitionAnimation/CustomClasses/AppTabBarController.swift
@@ -9,20 +9,17 @@
 import UIKit
 
 class AppTabBarController: UITabBarController {
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let viewOverTabBar = UIView(frame: CGRect(x: 0, y: UIScreen.main.bounds.height - tabBar.frame.size.height, width: UIScreen.main.bounds.width, height: 30));
-//        tabBar.superview!.addSubview(viewOverTabBar)
-//        viewOverTabBar.backgroundColor = .green
-//        viewOverTabBar.layer.borderWidth = 1
-//        viewOverTabBar.roundTopCorner(cornerRadius: 50)
-//        tabBar.superview!.addSubview(viewOverTabBar)
-//        viewOverTabBar.addShadow(shadowOffset: CGSize(width: 0, height: -4))
-//        viewOverTabBar.clipsToBounds = false
-//        tabBar.sendSubviewToBack(viewOverTabBar)
-        tabBar.roundTopCorner(cornerRadius: 50)
-        tabBar.clipsToBounds = false
-        tabBar.addShadow(shadowOffset: CGSize(width: 0, height: -10))
+        
+        //View for shadow
+        let viewOverTabBar = UIView(frame: CGRect(x: 0, y:UIScreen.main.bounds.height - (tabBar.frame.size.height), width: UIScreen.main.bounds.size.width, height: 50));
+        tabBar.superview!.addSubview(viewOverTabBar)
+        viewOverTabBar.addShadow(shadowOffset: CGSize(width: 0, height: -10))
+        viewOverTabBar.backgroundColor = .green
+        viewOverTabBar.layer.cornerRadius = 50
+        tabBar.superview!.insertSubview(viewOverTabBar, at: 1)
     }
 
 }
